ed86d44ebac13c1e86d00cac4a6621ed973c18dd
Mr-Tico89 <matias.rojas.v@ug.uchile.cl>
2025-01-20 21:24:36 -0300

    BREAKING CHANGE: start to implementate indicador semanal table

--- scripts.js
+++ scripts.js
@@ -32,21 +32,40 @@ document.addEventListener('DOMContentLoaded', function() {
     loadTableOptions();
 });
 
+// Agregar un evento `change` a todos los filtros
+document.querySelectorAll('.filter-select').forEach(select => {
+    select.addEventListener('change', (event) => {
+        const selectId = event.target.id.replace('filter-', ''); // ID del select
+        const value = Array.from(event.target.selectedOptions).map(option => option.value); // Valores seleccionados
+        updateSelectedFilters(selectId, value);
+    });
+});
 
 // Agregar eventos a los botones de pestañas
 document.addEventListener('DOMContentLoaded', async () => {
     document.querySelectorAll('.tablinks').forEach(button => {
         button.addEventListener('click', (event) => {
             const tabId = event.target.getAttribute('onclick').match(/'(.*?)'/)[1]; // Extrae el ID de la pestaña
+    
             switch (tabId) {
                 case 'Tab2': //carga la tabla db_averias_consolidado al entrar en la pestaña 2
-                    loadTableData('editable-container', 'Tab2', 'db_averias_consolidado');
+                    loadTableData('editable-container-tab2', 'Tab2', 'db_averias_consolidado', true);
                     break;
+
                 case 'Tab4': //carga la tabla indicador_semanal al entrar en la pestaña 4
-                    loadTableData('Indicador-container', 'Tab4', 'indicador_semanal');
+                    loadTableData('Indicador-container', 'Tab4', 'indicador_semanal', true);
+                    loadTableData('Indicador-container', 'Tab4', 'db_averias_consolidado', false)
+                    .then(data => {
+                        // Llamar a la función para cargar los datos y llenar los filtros
+                        loadAndPopulateFilters(data);
+                    })
+                    .catch(error => {
+                        console.error('Error al cargar los datos:', error);
+                    });
                     break;
+
                 case 'Tab6': //carga la tabla HPR_OEE al entrar en la pestaña 6
-                    loadTableData('OEE-container', 'Tab6', 'hpr_oee');
+                    loadTableData('editable-container-tab6', 'Tab6', 'hpr_oee', true);
                     break;
 
             }
@@ -57,6 +76,10 @@ document.addEventListener('DOMContentLoaded', async () => {
 
 // Objeto global para almacenar filtros activos por columna
 const globalActiveFilters  = {};
+
+
+// Objeto para almacenar los filtros seleccionados
+const selectedFilters = {};
 
 
 // Mostrar el botón cuando el usuario se desplaza hacia abajo 20px desde la parte superior del documento
@@ -118,45 +141,40 @@ async function loadTableOptions() {
 
 
 //funcion para cargar los datos del backtend al front
-async function loadTableData(containerId, tabName,  tableName = null) {
+async function loadTableData(containerId, tabName,  tableName = null, render) {
     const table = tableName || document.getElementById('table-select').value;
     if (!table) return;
 
-    const response = await fetch(`http://localhost:5000/tables/${table}`);
-    const data = await response.json();
+    try {
+        const response = await fetch(`http://localhost:5000/tables/${table}`);
+        const data = await response.json();
 
-    renderEditableTable(data, containerId);
-
+        if (render) {
+            renderEditableTable(data, containerId);
+        } else {
+            return data;
+        }
+    } catch (error) {
+        console.error(`Error fetching data for table ${table}:`, error);
+    }
 }
 
 
-// Crear el contenedor principal del filtro
-function createFilterContainer() {
+function createDropdownFilter() {
+    // Crear el contenedor principal del filtro
     const container = document.createElement('div');
     container.classList.add('filter-container');
-    return container;
-}
 
-
-// Crear un botón desplegable
-function createDropdownButton() {
+    // Crear el botón desplegable
     const dropdownButton = document.createElement('button');
     dropdownButton.textContent = '▼';
     dropdownButton.classList.add('dropdown-button');
-    return dropdownButton;
-}
 
-
-// Crear el menú desplegable
-function createDropdownMenu() {
+    // Crear el menú desplegable
     const dropdownMenu = document.createElement('div');
     dropdownMenu.classList.add('dropdown-menu');
-    return dropdownMenu;
-}
 
-
-// Manejar eventos para mostrar/ocultar el menú
-function setupDropdownEvents(dropdownButton, dropdownMenu) {
+    // Manejar eventos para mostrar/ocultar el menú
     dropdownButton.addEventListener('click', (event) => {
         event.stopPropagation();
         dropdownMenu.classList.toggle('show');
@@ -166,8 +184,14 @@ function setupDropdownEvents(dropdownButton, dropdownMenu) {
     dropdownMenu.addEventListener('mouseleave', () => {
         closeTimeout = setTimeout(() => dropdownMenu.classList.remove('show'), 75);
     });
-
     dropdownMenu.addEventListener('mouseenter', () => clearTimeout(closeTimeout));
+
+    // Añadir el botón y el menú al contenedor
+    container.appendChild(dropdownButton);
+    container.appendChild(dropdownMenu);
+
+    // Retornar un objeto con los elementos clave
+    return { container, dropdownButton, dropdownMenu };
 }
 
 
@@ -198,15 +222,10 @@ function createClearButton(column, dropdownMenu, columns, table) {
 
 // Función para crear el selector de filtros
 function createFilterSelect(column, data, columns, table) {
-    const container = createFilterContainer();
-    const dropdownButton = createDropdownButton();
-    const dropdownMenu = createDropdownMenu();
+    const {container, dropdownButton, dropdownMenu} = createDropdownFilter();
 
     // Inicializar filtros activos para esta columna si no existen
     globalActiveFilters[column] = globalActiveFilters[column] || new Set();
-
-    // Configurar eventos del menú desplegable
-    setupDropdownEvents(dropdownButton, dropdownMenu);
 
     // Opciones únicas
     const uniqueValues = [...new Set(data.map(row => row[column]))];
@@ -220,7 +239,7 @@ function createFilterSelect(column, data, columns, table) {
 
         const label = document.createElement('span');
         label.textContent = value;
-
+        
         checkbox.addEventListener('change', () => {
             if (checkbox.checked) {
                 globalActiveFilters[column].add(value);
@@ -257,7 +276,7 @@ function applyFilters(columns, table) {
         let shouldDisplay = true;
         columns.forEach(column => {
             if (globalActiveFilters[column] && globalActiveFilters[column].size > 0) {
-                const cell = row.querySelector(`td:nth-child(${columns.indexOf(column) + 1})`);
+                const cell = row.querySelector(`td:nth-child(${columns.indexOf(column) + 2})`);
                 let cellValue = '';
 
                 // Verificar si la celda contiene un input (para valores editables)
@@ -275,64 +294,6 @@ function applyFilters(columns, table) {
         });
         row.style.display = shouldDisplay ? '' : 'none';
     });
-}
-
-
-//funcion de las celdas editables
-function createEditableCell(row, column, data, rowIndex) {
-    const td = document.createElement('td');
-    const cellValue = row[column] || ''; // Handle missing values
-    
-    if (column === 'areas' && cellValue !== 'Paros Menores') {
-        const select = document.createElement('select');
-        const options = ['', 'Mecánico', 'Eléctrico'];
-
-        options.forEach(optionValue => {
-            const option = document.createElement('option');
-            option.value = optionValue;
-            option.textContent = optionValue;
-            
-            if (cellValue === optionValue) {
-                option.selected = true;
-            }
-            select.appendChild(option);
-        });
-        select.addEventListener('change', (e) => {
-            data[rowIndex][column] = e.target.value;
-            saveData(data);
-        });
-        td.appendChild(select);
-    } 
-
-    // Solo permitir edición en las columnas 'sintoma' y 'observaciones'
-    else if (column === 'sintoma' || column === 'observaciones') {
-        // Crear un input editable
-        const input = document.createElement('input');
-        input.type = 'text';
-        input.placeholder = 'Editar'
-        input.value = cellValue; // Valor inicial
-
-        // Forzar mayúsculas solo en la columna 'observaciones'
-        if (column === 'observaciones') {
-            input.addEventListener('input', () => {
-                input.value = input.value.toUpperCase();
-            });
-        }
-
-        // Guardar cambios al perder el foco
-        input.addEventListener('blur', () => {
-            data[rowIndex][column] = input.value;
-            saveData(data)
-        });
-
-        td.appendChild(input);
-    }
-    else {
-
-        td.textContent = cellValue; // Valor predeterminado
-    }
-
-    return td;
 }
 
 
@@ -374,9 +335,8 @@ function createTableHeader(columns, containerId, data, table, globalActiveFilter
     thead.classList.add('editable-table-header');
     const headerRow = thead.insertRow();
 
-
     const buttonHeader = createHeaderCell('Quitar');
-    if (containerId !== 'editable-container') { // ocultar la columna quitar para que filten bien los datos
+    if (!containerId.includes('editable-container'))  { // ocultar la columna quitar para que filten bien los datos
         buttonHeader.classList.add('hidden-column');
     }
     headerRow.appendChild(buttonHeader);
@@ -417,14 +377,14 @@ function createTableBody(columns, data, containerId, globalActiveFilters, table)
         const tr = tbody.insertRow();
 
         const buttonCell = createButtonCell(tr, tbody, data);
-        if (containerId !== 'editable-container') { // ocultar la columna quitar para que filten bien los datos
+        if (!containerId.includes('editable-container')) { // ocultar la columna quitar para que filten bien los datos
             buttonCell.classList.add('hidden-column');
         }
         tr.appendChild(buttonCell);
         
 
         columns.forEach(column => {
-            const td = containerId === 'editable-container'
+            const td = containerId.includes('editable-container')
                 ? createEditableCell(row, column, data, rowIndex)
                 : createTextCell(row[column]);
             tr.appendChild(td);
@@ -437,6 +397,64 @@ function createTableBody(columns, data, containerId, globalActiveFilters, table)
     });
 
     return tbody;
+}
+
+
+//funcion de las celdas editables
+function createEditableCell(row, column, data, rowIndex) {
+    const td = document.createElement('td');
+    const cellValue = row[column] || ''; // Handle missing values
+    
+    if (column === 'areas' && cellValue !== 'Paros Menores') {
+        const select = document.createElement('select');
+        const options = ['', 'Mecánico', 'Eléctrico'];
+
+        options.forEach(optionValue => {
+            const option = document.createElement('option');
+            option.value = optionValue;
+            option.textContent = optionValue;
+            
+            if (cellValue === optionValue) {
+                option.selected = true;
+            }
+            select.appendChild(option);
+        });
+        select.addEventListener('change', (e) => {
+            data[rowIndex][column] = e.target.value;
+            saveData(data);
+        });
+        td.appendChild(select);
+    } 
+
+    // Solo permitir edición en las columnas 'sintoma' y 'observaciones'
+    else if (column === 'sintoma' || column === 'observaciones' || column === 'OEE_(%)') {
+        // Crear un input editable
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.placeholder = 'Editar'
+        input.value = cellValue; // Valor inicial
+
+        // Forzar mayúsculas solo en la columna 'observaciones'
+        if (column === 'observaciones') {
+            input.addEventListener('input', () => {
+                input.value = input.value.toUpperCase();
+            });
+        }
+
+        // Guardar cambios al perder el foco
+        input.addEventListener('blur', () => {
+            data[rowIndex][column] = input.value;
+            saveData(data)
+        });
+
+        td.appendChild(input);
+    }
+    else {
+
+        td.textContent = cellValue; // Valor predeterminado
+    }
+
+    return td;
 }
 
 
@@ -530,3 +548,138 @@ function saveData(data) {
 }
 
 
+
+
+// Función para actualizar los filtros seleccionados
+function updateSelectedFilters(selectId, value) {
+    if (value.length > 0) {
+        selectedFilters[selectId] = value; // Actualiza el valor seleccionado
+    } else {
+        delete selectedFilters[selectId]; // Elimina el filtro si no hay selección
+    }
+
+    // Convertir el objeto a formato JSON
+    const filtersJSON = JSON.stringify(selectedFilters);
+    console.log(filtersJSON); // Imprime el JSON en la consola para su uso
+}
+
+
+function populateUniqueSelect(json, property, selectId) {
+    const select = document.getElementById(selectId);
+
+    // Verificar si el select existe
+    if (!select) {
+        console.error(`No se encontró el elemento con ID: ${selectId}`);
+        return;
+    }
+
+    // Verificar que json.data exista y sea un array
+    if (!json || !json.data || !Array.isArray(json.data)) {
+        console.error("La estructura del JSON no es válida o no contiene 'data'. Verifica la entrada:", json);
+        return;
+    }
+
+    // Verificar que la propiedad esté en los objetos de "data"
+    if (!json.data[0].hasOwnProperty(property)) {
+        console.error(`La propiedad '${property}' no existe en los objetos de 'data'.`);
+        return;
+    }
+
+    // Extraer valores únicos de la propiedad
+    const uniqueValues = [...new Set(json.data.map(item => item[property]))];
+
+    // Limpiar las opciones previas antes de agregar nuevas
+    select.innerHTML = '';
+
+    // Agregar el atributo multiple si no existe
+    select.setAttribute('multiple', 'true');
+
+    // Agregar las nuevas opciones al select
+    uniqueValues.forEach(value => {
+        //if (value !== '') { // Verificar que el valor no sea ''
+            const option = document.createElement('option');
+            option.value = value;
+            option.textContent = value;
+            select.appendChild(option);
+        //}
+    });
+}
+
+
+async function loadAndPopulateFilters(data) {
+    // Llenar los filtros con los datos obtenidos
+    populateUniqueSelect(data, 'año', 'filter-año'); // Filtro Año
+    populateUniqueSelect(data, 'mes', 'filter-mes'); // Filtro Mes
+    populateUniqueSelect(data, 'semana', 'filter-semana'); // Filtro Semana
+    populateUniqueSelect(data, 'areas', 'filter-areas'); // Filtro Semana
+}
+
+
+function getSelectedValues(selectId) {
+    const select = document.getElementById(selectId);
+    const selectedValues = [...select.selectedOptions].map(option => option.value);
+    return selectedValues;
+}
+
+
+// Función para aplicar los filtros y realizar cálculos
+function applyFiltersAndCalculate() {
+    loadTableData('Indicador-container', 'Tab4', 'db_averias_consolidado', false)
+    .then(json => {
+        if (!json || !json.data) {
+            console.error("Los datos de la tabla no son válidos.");
+            return;
+        }
+
+        // Filtrar los datos según los filtros seleccionados
+        const filteredData = json.data.filter(row => {
+            return Object.entries(selectedFilters).every(([key, values]) => {
+                const rowValue = String(row[key] || ""); // Asegurarse de que el valor sea una cadena
+                return values.includes(rowValue); // Comparar si el valor de la fila está en los valores seleccionados
+            });
+        });
+
+        // Si no hay datos filtrados, mostrar un mensaje de advertencia
+        if (filteredData.length === 0) {
+            console.warn("No se encontraron datos que coincidan con los filtros.");
+        }
+
+        // Valores predeterminados para minutos por id (columna minutos de la tabla)
+        let minutos = {"L1": 0, "L2": 0, "L3": 0, "L4": 0, "PLANTA": 0};
+
+        // Filtrar los datos por valores únicos de "id" y luego sumar los minutos
+        const uniqueIds = [...new Set(filteredData.map(row => row.id))]; // Extrae los valores únicos de "id"
+
+        // Sumar los minutos de cada "id" único
+        uniqueIds.forEach(id => {
+            // Filtrar las filas para cada id único
+            const rowsForId = filteredData.filter(row => row.id === id);
+            const totalMinutesForId = rowsForId.reduce((sum, row) => sum + (parseFloat(row.minutos) || 0), 0);
+
+            // Actualizar los minutos en el objeto minutos
+            if (minutos.hasOwnProperty(id)) {
+                minutos[id] = totalMinutesForId;
+            } else {
+                minutos[id] = totalMinutesForId; // Si el id no está, lo añadimos dinámicamente
+            }
+        });
+
+        // Sumar el total de minutos de todos los id y actualizar "TOTAL"
+        const totalMinutes = Object.values(minutos).reduce((sum, value) => sum + value, 0);
+        minutos["PLANTA"] = totalMinutes; // Guardamos el total global
+
+        console.log("Total de minutos por ID:", minutos);
+    })
+    .catch(error => {
+        console.error("Error al cargar los datos:", error);
+    });
+}
+
+
+
+
+
+// Agregar el evento al botón "Generar Datos"
+document.getElementById('Gen-button').addEventListener('click', () => {
+    applyFiltersAndCalculate();
+});
